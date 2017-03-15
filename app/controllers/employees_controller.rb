f538842c311a20dc2e7d0e449322c38bab530b59
Anitha Narayanasamy <anishakthi@gmail.com>
2017-03-15 11:20:03 -0700

    adding add feature and listing employee fetures

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,4 +1,27 @@
 class EmployeesController < ApplicationController
 	def new 
 	end
+
+	def index
+	@employee = Employee.all
+	end
+
+	def show
+		@employee = Employee.find(params[:id]) 
+	end
+
+	# Action method to Add Employee
+	def create 
+
+		@employee = Employee.new(add_employee_request)
+
+		# returns boolean 
+		@employee.save
+		redirect_to @employee
+	end
+
+	def add_employee_request
+		params.require(:employee).permit(:name, :doj, :status, :dob)
+	end
+
 end
